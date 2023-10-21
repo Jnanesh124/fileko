1e609c8bf6983639d2ec9d4eb2cdbda833cb46e8
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2023-10-21 23:47:07 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐠𝐫𝐨𝐮𝐩", url = f"https://t.me/ROCKERSBACKUP"),
-                    InlineKeyboardButton("𝐌𝐗𝐏𝐋𝐀𝐘𝐄𝐑 𝐦𝐨𝐯𝐢𝐞  📡", url = f"https://t.me/+tU8INwk_RbRlYWM1")
+                    InlineKeyboardButton("𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐠𝐫𝐨𝐮𝐩", url = f"https://t.me/+Pg5xeggLye5lZDA1"),
+                    InlineKeyboardButton("Ott 𝐦𝐨𝐯𝐢𝐞  📡", url = f"https://t.me/+D7L-rX9lKA43MGRl")
                 ]
             ]
         )
